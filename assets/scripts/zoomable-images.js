073d727369541f714043eae514f810ccdee084e0
Ella Hung <carnationreco@gmail.com>
2026-08-11 17:46:04 +1000

    edit responsiveness of project contents, play button placed behind the window, background image change

--- assets/scripts/zoomable-images.js
+++ assets/scripts/zoomable-images.js
@@ -1,15 +1,14 @@
-const modal = document.getElementById("imgModal");
-const modalImg = document.getElementById("imgModalContent");
-document.querySelectorAll(".zoomable").forEach(img => {
-  img.addEventListener("click", () => {
-    modalImg.src = img.src;
-    modal.classList.remove("hidden");
-    document.body.style.overflow = "hidden";
-  });
-});
+// const modal = document.getElementById("imgModal");
+// const modalImg = document.getElementById("imgModalContent");
+// document.querySelectorAll(".zoomable").forEach(img => {
+//   img.addEventListener("click", () => {
+//     modalImg.src = img.src;
+//     modal.classList.remove("hidden");
+//     document.body.style.overflow = "hidden";
+//   });
+// });
 
-modal.addEventListener("click", () => {
-  // remove this if you truly never want it to close
-  modal.classList.add("hidden");
-  document.body.style.overflow = "";
-});
+// modal.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   document.body.style.overflow = "";
+// });
